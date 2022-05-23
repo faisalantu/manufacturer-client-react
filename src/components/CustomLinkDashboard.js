dd544ed7ab4d855eb69e10c978e48a5a8b36da45
faisalantu <faisalantu@gmail.com>
2022-05-24 04:48:20 +0600

    😎 custom link created

--- src/components/CustomLinkDashboard.js
+++ src/components/CustomLinkDashboard.js
@@ -8,7 +8,7 @@ function CustomLink({ children, to, ...props }) {
     return (
       <div>
         <Link
-          className={`${match?"text-rose-600 font-semibold":null}`}
+          className={`${match?"text-rose-600 font-semibold border-b border-stone-400":null}`}
           to={to}
           {...props}
         >
