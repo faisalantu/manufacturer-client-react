815aa59d60c2b7aec3887023d07a1aa938ceff76
faisalantu <faisalantu@gmail.com>
2022-05-24 10:05:35 +0600

    🐈‍⬛ all product page done

--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -5,20 +5,29 @@ import toast from "react-hot-toast";
 import DeleteModal from "../components/DeleteModal";
 import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
+import { useQuery } from "react-query";
 
 const AllProducts = () => {
   // eslint-disable-next-line
-  const [fetchingProduct, setFetchingProduct] = useState(false);
-  const [products, setProducts] = useState([]);
   const [page, setPage] = useState(0);
   const [totalPage, setTotalPage] = useState(0);
+  const [fetchingProduct, setFetchingProduct] = useState(false);
 
-  const fetchProducts = async () => {
+  const {
+    isLoading,
+    data: products,
+    refetch,
+  } = useQuery("products", async () => {
+    const res = await axios.get(`/product?skip=${page * 10}`);
+    return res.data;
+  });
+
+  const fetchProductCount = async () => {
     try {
       setFetchingProduct(true);
-      const res = await axios.get(`/product?skip=${page * 10}`);
+      const res = await axios.get(`/product/productcount`);
       setFetchingProduct(false);
-      setProducts(res.data);
+      setTotalPage(res.data.totalProducts);
     } catch (err) {
       setFetchingProduct(false);
       toast.error("Something went wrong try again");
@@ -26,43 +35,36 @@ const AllProducts = () => {
   };
 
   useEffect(() => {
-    fetchProducts();
+    refetch();
     // eslint-disable-next-line
   }, [page]);
   useEffect(() => {
-    (async function () {
-      try {
-        setFetchingProduct(true);
-        const res = await axios.get(`/product/productcount`);
-        setFetchingProduct(false);
-        setTotalPage(res.data.totalProducts);
-      } catch (err) {
-        setFetchingProduct(false);
-        toast.error("Something went wrong try again");
-      }
-    })();
+    fetchProductCount();
     // eslint-disable-next-line
   }, []);
 
   return (
     <>
-      {fetchingProduct ? (
+      {fetchingProduct || isLoading ? (
         <Loading />
       ) : (
-        <div className='my-10'>
-          <table className=' table-fixed w-full lg:w-9/12 md:mx-auto text-sm text-left text-gray-500 dark:text-gray-300 shadow-lg rounded-lg overflow-hidden border'>
+        <div className='my-10 overflow-x-auto'>
+          <table className='table w-full lg:w-9/12 md:mx-auto text-sm text-left text-gray-500 dark:text-gray-300 shadow-lg rounded-lg  border'>
             <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300'>
               <tr>
                 <th scope='col' className='px-6 py-3'>
+                  No
+                </th>
+                <th scope='col' className='px-6 py-3'>
                   Product name
                 </th>
-                <th scope='col' className='px-6 py-3 hidden md:block'>
+                <th scope='col' className='px-6 py-3'>
                   Price
                 </th>
                 <th scope='col' className='px-6 py-3'>
                   Quantity
                 </th>
-                <th scope='col' className='px-6 py-3  hidden md:block'>
+                <th scope='col' className='px-6 py-3 '>
                   user
                 </th>
                 <th scope='col' className='px-6 py-3 text-center'>
@@ -72,7 +74,7 @@ const AllProducts = () => {
             </thead>
             <tbody>
               {products
-                ? products.map((product) => {
+                ? products.map((product, index) => {
                     return (
                       <tr
                         key={product._id}
@@ -82,20 +84,26 @@ const AllProducts = () => {
                           scope='row'
                           className='px-6 py-4 font-medium text-gray-900 dark:text-white'
                         >
+                          {index+1}
+                        </th>
+                        <th
+                          scope='row'
+                          className='px-6 py-4 font-medium text-gray-900 dark:text-white'
+                        >
                           {product?.title && product?.title}
                         </th>
-                        <td className='px-6 py-4 hidden md:block '>
+                        <td className='px-6 py-4 '>
                           ${product?.price && product?.price}
                         </td>
                         <td className='px-6 py-4'>
                           {product?.quantity && product?.quantity}
                         </td>
-                        <td className='px-6 py-4  hidden md:block'>
+                        <td className='px-6 py-4 '>
                           {product?.displayName && product?.displayName}
                         </td>
                         <td className='px-6 py-4 text-right'>
                           <Link
-                            to={`/inventory/${product._id}`}
+                            to={`/dashboard/editproduct/${product._id}`}
                             className='btn btn-xs'
                           >
                             <div className='flex justify-center items-center'>
@@ -104,7 +112,7 @@ const AllProducts = () => {
                             </div>
                           </Link>
                           <DeleteModal
-                            fetchProducts={fetchProducts}
+                            fetchProducts={refetch}
                             pid={product._id}
                           />
                         </td>
@@ -120,19 +128,17 @@ const AllProducts = () => {
                 {/* {pageContext.currentPage > 1 ? "<<Prev" : null} */}
                 {Array.from({ length: Math.ceil(totalPage / 10) }, (_, i) => {
                   return (
-                    <>
-                      <button
+                    <button
                       key={i}
-                        onClick={() => {
-                          setPage(i);
-                        }}
-                        className={`btn ${
-                          i + 1 === page + 1 ? "btn-active" : ""
-                        } `}
-                      >
-                        {i + 1}
-                      </button>
-                    </>
+                      onClick={() => {
+                        setPage(i);
+                      }}
+                      className={`btn ${
+                        i + 1 === page + 1 ? "btn-active" : ""
+                      } `}
+                    >
+                      {i + 1}
+                    </button>
                   );
                 })}
               </div>
