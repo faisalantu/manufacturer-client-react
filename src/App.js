dee80a31a750695c7af22e1bec7a2465cb96ea6c
faisalantu <faisalantu@gmail.com>
2022-05-24 10:07:04 +0600

    💀 Not found page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+// import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import AddProduct from "./pages/AddProduct";
+import Dashboard from './pages/Dashboard';
+import AllProducts from './pages/AllProducts';
+import AuthRequired from "./components/AuthRequired";
+import EditProduct from './pages/EditProduct';
+import AdminAuthRequired from './components/AdminAuthRequired';
+import AllUsers from './pages/AllUsers';
+import Profile from './pages/Profile';
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Toaster />
+      <div className='container mx-auto px-5 md:px-0'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='register' element={<Register />}></Route>
+          <Route path='dashboard' element={<AuthRequired><Dashboard /></AuthRequired>}>
+            <Route index path='profile' element={<AdminAuthRequired><Profile /></AdminAuthRequired>}></Route>
+            <Route path='addproduct' element={<AdminAuthRequired><AddProduct /></AdminAuthRequired>}></Route>
+            <Route path='allproducts' element={<AdminAuthRequired><AllProducts /></AdminAuthRequired> }></Route>
+            <Route path='manageadmins' element={<AdminAuthRequired><AllUsers /></AdminAuthRequired> }></Route>
+            <Route path='editproduct/:id' element={<AdminAuthRequired><EditProduct /></AdminAuthRequired>}></Route>
+          </Route>
+          <Route path='*' element={<NotFound />}></Route> 
+        </Routes>
+      </div>
     </div>
   );
 }
