953355940f4c8eb964d7075605ae3b16f944cb8c
faisalantu <faisalantu@gmail.com>
2022-05-26 17:34:46 +0600

    💸 payment and checkout done

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import axios from "../axiosConfig";
+
+const CheckoutForm = ({ order, price }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
+  const { _id, email, displayName } = order;
+
+
+
+  useEffect(() => {
+    const createIntent = async () => {
+      const res = await axios.patch("/payment/create-payment-intent", {
+        price: price,
+      });
+      console.log(res.data);
+      if (res.data?.clientSecret) {
+        setClientSecret(res.data.clientSecret);
+      }
+    };
+    createIntent()
+  }, [price]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    setCardError(error?.message || "");
+    setSuccess("");
+    setProcessing(true);
+    // confirm card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: displayName,
+            email: email,
+          },
+        },
+      });
+
+    if (intentError) {
+      setCardError(intentError?.message);
+      setProcessing(false);
+    } else {
+      setCardError("");
+      setTransactionId(paymentIntent.id);
+      console.log(paymentIntent);
+      setSuccess("Congrats! Your payment is completed.");
+
+      //store payment on database
+      const payment = {
+        bookingId: _id,
+        transactionId: paymentIntent.id,
+      };
+
+
+      const res = axios.patch(`order/payment?productId=${_id}`,payment)
+      setProcessing(false);
+      console.log(res.data);
+
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className='btn btn-success btn-sm mt-4'
+          type='submit'
+          disabled={!stripe || !clientSecret || success}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className='text-red-500'>{cardError}</p>}
+      {success && (
+        <div className='text-green-500'>
+          <p>{success} </p>
+          <p>
+            Your transaction Id:{" "}
+            <span className='text-orange-500 font-bold'>{transactionId}</span>{" "}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CheckoutForm;
---
+++ src/pages/Payment.js
@@ -0,0 +1,52 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+
+// import CheckoutForm from './CheckoutForm';
+import axios from '../axiosConfig';
+import Loading from './../components/Loading';
+import CheckoutForm from './../components/CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_TcGkWcoku08cu5mp45b6dHvw00bEnG8iop');
+
+const Payment = () => {
+    const { id } = useParams();
+
+    const {
+      isLoading,
+      data: order,
+      refetch,
+    } = useQuery("payOrder", async () => {
+      const res = await axios.get(`/order/one?productId=${id}`);
+      console.log(res.data[0]);
+      return res.data[0];
+    });
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    return (
+        <div>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className="text-success font-bold">Hello, {order.displayName}</p>
+                    <h2 class="card-title">Please Pay for <span className='font-semibold text-gray-600'>{order.title}</span></h2>
+                    <p>Quantity: <span className='text-orange-700'>{order.quantity}</span></p>
+                    <p>Please pay: ${+order.price * +order.quantity}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm order={order} price={+order.price * +order.quantity} />
+                    </Elements>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
